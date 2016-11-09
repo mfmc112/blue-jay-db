7379ed502a9cbcc4acb78f04d64ea0048ac4bd3b
Marcos Costa <mfmc112@gmail.com>
2016-11-08 19:45:33 -0600

    created unit test for SearchAggregator

--- src/test/java/com/bluejay/core/search/SearchAggregatorTest.java
+++ src/test/java/com/bluejay/core/search/SearchAggregatorTest.java
@@ -1,45 +1,69 @@
 package com.bluejay.core.search;
 
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
 import org.junit.Assert;
 import org.junit.Test;
 
+import com.bluejay.core.exception.FCException;
+import com.bluejay.logger.Logger;
+
+@SuppressWarnings("unchecked")
 public class SearchAggregatorTest {
 
+	Logger log = new Logger(SearchAggregatorTest.class);
+	
 	@Test
 	public void aggregateAndTest() {
-		Set<Long> set1 = new HashSet<Long>();
-		set1.add(2L);
-		set1.add(4L);
-		set1.add(6L);
-		set1.add(8L);
-		Set<Long> set2 = new HashSet<Long>();
-		set2.add(1L);
-		set2.add(3L);
-		set2.add(4L);
-		set2.add(5L);
-		set2.add(8L);
-		Set<Long> set3 = new HashSet<Long>();
-		set3.add(1L);
-		set3.add(3L);
-		set3.add(4L);
-		set3.add(5L);
+		Long[] l1 = new Long[]{2L,4L,6L,8L};
+		Set<Long> set1 = new HashSet<Long>(Arrays.asList(l1));
+		
+		Long[] l2 = new Long[]{1L,3L,4L,5L,8L};
+		Set<Long> set2 = new HashSet<Long>(Arrays.asList(l2));
+		
+		Long[] l3 = new Long[]{1L,3L,4L,5L};
+		Set<Long> set3 = new HashSet<Long>(Arrays.asList(l3));
+
 		SearchAggregator s = new SearchAggregator();
 		Set<Long> aggregatedSet = s.aggregateAnd(set1, set2, set3);
-		Assert.assertEquals("[4]", aggregatedSet.toString());
+
+		Assert.assertEquals(1, aggregatedSet.size());
+		Assert.assertTrue(aggregatedSet.contains(4L));
 	}
 	
 	@Test
 	public void aggregateAndSingleSetTest() {
-		Set<Long> set1 = new HashSet<Long>();
-		set1.add(2L);
-		set1.add(4L);
-		set1.add(6L);
-		set1.add(8L);
+		Long[] l1 = new Long[]{2L,4L,6L,8L};
+		Set<Long> set1 = new HashSet<Long>(Arrays.asList(l1));
 		SearchAggregator s = new SearchAggregator();
 		Set<Long> aggregatedSet = s.aggregateAnd(set1);
-		Assert.assertEquals("[2,4,6,8]", aggregatedSet.toString());
+		
+		Assert.assertEquals(4, aggregatedSet.size());
+		Assert.assertTrue(aggregatedSet.contains(2L));
+		Assert.assertTrue(aggregatedSet.contains(4L));
+		Assert.assertTrue(aggregatedSet.contains(6L));
+		Assert.assertTrue(aggregatedSet.contains(8L));
+	}
+	
+	@Test
+	public void aggregateAnd100kTime() throws FCException {
+		Set<Long> set1 = randomSet(0, 100000);
+		Set<Long> set2 = randomSet(7000, 100000);
+		log.startTimer();
+		SearchAggregator s = new SearchAggregator();
+		Set<Long> aggregatedSet = s.aggregateAnd(set1);
+		
+		// It needs to run in less then 10 milliseconds. Yes 1/100 of a millisecond
+		Assert.assertTrue(log.endTimer() <=10);
+	}
+	
+	private Set<Long> randomSet(int startAt, int size) {
+		Set<Long> set = new HashSet<Long>();
+		for(int i = startAt; i < (startAt + size); i++) {
+			set.add(new Long(i));
+		}
+		return set;
 	}
 }
